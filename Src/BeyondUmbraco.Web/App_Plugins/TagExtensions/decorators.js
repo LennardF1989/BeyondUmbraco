9bd805c45c79eb9a86a37ceb6507e05faea64b14
Lennard Fonteijn <lennard@lennardf1989.com>
2021-02-25 13:53:15 +0100

    Added TagExtensions

---
+++ Src/BeyondUmbraco.Web/App_Plugins/TagExtensions/decorators.js
@@ -0,0 +1,44 @@
+﻿angular.module("umbraco.directives").config(function ($provide) {
+    $provide.decorator("umbTagsEditorDirective", function ($delegate) {
+        var directive = $delegate[0];
+
+        var originalController = directive.controller;
+
+        directive.controller = function ($rootScope, assetsService, umbRequestHelper, angularHelper, $timeout, $element) {
+            //originalController.call(this, $rootScope, assetsService, umbRequestHelper, angularHelper, $timeout, $element);
+            originalController.apply(this, arguments);
+
+            //NOTE: Copied from umbtagseditor.directive.js
+            this.addTagOnEnter = function(e) {
+                var code = e.keyCode || e.which;
+
+                if (code === 13) {
+                    var vm = this;
+
+                    if ($element.find(".tags-" + vm.inputId).parent().find(".tt-menu .tt-cursor").length === 0) {
+                        e.preventDefault();
+
+                        vm.addTag();
+                    }
+                }
+            }
+
+            var originalAddTag = this.addTag;
+
+            this.addTag = function() {
+                var vm = this;
+
+                vm.tagToAdd
+                    .split(" ")
+                    .forEach(x => addSingleTag(x));
+
+                function addSingleTag(x) {
+                    vm.tagToAdd = x;
+                    originalAddTag();
+                }
+            };
+        };
+
+        return $delegate;
+    });
+});
